e7257122000d403cd556e08766fcf11e84d7b423
Mohammed El Alladi <moal@eaaa.dk>
2025-09-09 10:33:47 +0200

    eat(registration): Registration form

--- GitflowConsoleEfterår2025/Program.cs
+++ GitflowConsoleEfterår2025/Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!"  );   
+        Console.WriteLine("Hello, World!"); 
+        Console.WriteLine("Hej Feature2_Registration");  
     }
 }
