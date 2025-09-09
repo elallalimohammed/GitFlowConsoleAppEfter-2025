b9960de60fd2c4d3fb25db32167080c7ab801a14
Mohammed El Alladi <moal@eaaa.dk>
2025-09-09 09:12:34 +0200

    Initial commit

---
+++ GitflowConsoleEfterår2025/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!"  );   
+    }
+}
